6940a82243ef40c42481c44abfc79c192e5c480c
giovanni-arduini <gionnibigudd@gmail.com>
2024-12-12 12:23:51 +0100

    ADD fetchMedia function

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,24 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
 import GlobalContext from "./Context/GlobalContext";
 import CatalogSearch from "./Components/CatalogSearch/CatalogSearch";
+import axios from "axios";
 
 function App() {
-  const [movieList, setMovieList] = useState([]);
+  const [mediaList, setMediaList] = useState([]);
+
+  function fetchMedia() {
+    axios
+      .get(
+        "https://api.themoviedb.org/3/search/tv?api_key=138655685094138af87852193ddccc85&query=scrubs"
+      )
+      .then((res) => setMediaList(res.data.reults));
+  }
+
+  useEffect(() => {
+    fetchMedia();
+  }, []);
 
   return (
     <>
