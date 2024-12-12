6842f848eaa63ab99462ee5f0d9acf2602898242
giovanni-arduini <gionnibigudd@gmail.com>
2024-12-12 12:12:30 +0100

    ADD Route to CatalogSearch component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
 import { useState } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
 import GlobalContext from "./Context/GlobalContext";
+import CatalogSearch from "./Components/CatalogSearch/CatalogSearch";
 
 function App() {
   return (
     <>
-      <GlobalContext.Provider></GlobalContext.Provider>
+      <GlobalContext.Provider value="">
+        <BrowserRouter>
+          <Routes>
+            <Route path="" Component={CatalogSearch}></Route>
+          </Routes>
+        </BrowserRouter>
+      </GlobalContext.Provider>
     </>
   );
 }
