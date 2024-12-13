477676146cc51269b3f35437cff809bb6a012fb6
giovanni-arduini <gionnibigudd@gmail.com>
2024-12-13 10:35:16 +0100

    ADD ItamList component

--- src/Components/ItemList/ItemList.jsx
+++ src/Components/ItemList/ItemList.jsx
@@ -2,11 +2,25 @@
 
 // Metteremo le card popolate dai dati
 
-function ItemList() {
+import Card from "../Card/card";
+
+function ItemList(items = [], title) {
   return (
-    <>
-      <p>"Item List"</p>
-    </>
+    <section>
+      <div className="container">
+        <h2>{title}</h2>
+      </div>
+      <div className="container">
+        <ul className="grid">
+          {items.map((item) => (
+            <li kay={item.id}>
+              <Card item={item} />
+            </li>
+          ))}
+        </ul>
+        ;
+      </div>
+    </section>
   );
 }
 
