b8d7ba16e1c271e75d4e9679dd1e112b776b4210
giovanni-arduini <gionnibigudd@gmail.com>
2024-12-13 10:18:32 +0100

    ADD Card component elements

--- src/Components/Card/Card.jsx
+++ src/Components/Card/Card.jsx
@@ -1,10 +1,15 @@
 // qui stamperemo effettivamente i risultati
 
-function Card() {
+function Card({ item }) {
+  const { title, original_title, original_language, vote_average } = item;
+
   return (
-    <>
-      <p>"Card"</p>
-    </>
+    <div>
+      <h3>{title}</h3>
+      <p>{original_title}</p>
+      <p>{original_language}</p>
+      <p>{vote_average}</p>
+    </div>
   );
 }
 
