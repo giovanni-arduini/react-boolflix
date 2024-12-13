74b35b6cf6bf661472fda82bb1996ebdcbf98c46
giovanni-arduini <gionnibigudd@gmail.com>
2024-12-13 12:02:40 +0100

    ADD img poster_path to Card

--- src/Components/Card/Card.jsx
+++ src/Components/Card/Card.jsx
@@ -13,8 +13,17 @@ function Card({ item }) {
     poster_path,
   } = item;
 
+  const BASE_POSTER_URI = "https://image.tmdb.org/t/p/";
+
+  const posterSizes = {
+    sm: "w92",
+    md: "w185",
+    lg: "500",
+  };
+
   return (
     <div>
+      <img src={`${BASE_POSTER_URI}${posterSizes.sm}${poster_path}`} alt="" />
       <h3>{title || name}</h3>
       <p>{original_title || original_name}</p>
 
